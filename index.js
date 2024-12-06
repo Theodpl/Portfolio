ab3c269dcb7f4966fd6adf7f04ef2b27a8049077
Theodpl <contact.theodpl@gmail.com>
2024-12-06 15:15:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-function previous() {
-    const widthSlider = document.querySelector('.slider-content').offsetWidth;
-    document.querySelector('.slider-content').scrollLeft -= widthSlider;
-}
-function next() {
-    const widthSlider = document.querySelector('.slider-content').offsetWidth;
-    document.querySelector('.slider-content').scrollLeft += widthSlider;
-}
+document.addEventListener("DOMContentLoaded", () => {
+    // Sélectionne toutes les sliders
+    const sliders = document.querySelectorAll(".slider");
 
-// document.querySelectorAll('.slider-content').forEach(button) => {
+    sliders.forEach((slider) => {
+        const sliderContent = slider.querySelector(".slider-content");
+        const prevButton = slider.querySelector(".bouton-slider-prev");
+        const nextButton = slider.querySelector(".bouton-slider-next");
 
-// }
+        prevButton.addEventListener("click", () => {
+            const widthSlider = sliderContent.offsetWidth;
+            sliderContent.scrollLeft -= widthSlider;
+        });
+
+        nextButton.addEventListener("click", () => {
+            const widthSlider = sliderContent.offsetWidth;
+            sliderContent.scrollLeft += widthSlider;
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logo = document.querySelector(".logo-theo-cta");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          logo.classList.add("visible");
+        } else {
+          logo.classList.remove("visible");
+        }
+      });
+    });
+  
+    observer.observe(logo);
+  });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const logoHeader = document.querySelector(".logo-theo-header");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          logoHeader.classList.add("visible");
+        } else {
+          logoHeader.classList.remove("visible");
+        }
+      });
+    });
+  
+    observer.observe(logoHeader);
+  });
